848d80d73448cc8aacf839be941bd4f31cd91b39
km-rjun <mail.rjun.km@gmail.com>
2025-08-15 15:39:05 +0530

    made rock-paper-scissor card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
 import TopBar from "../../components/TopBar"
+import Cards from "../../components/Cards"
 
 export default function Home() {
-    return <TopBar /> 
+   const cards = [
+    {
+     title: "Rock Paper Scissor", imageSrc: "/rpc.png" },
+  ];
+    return (
+        <>
+        <TopBar /> 
+        <Cards items={cards} />
+    </>
+    )
 }
